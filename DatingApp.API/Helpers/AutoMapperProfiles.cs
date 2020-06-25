f2db96e70531b689adf7e27dd5a091c7d41c4832
Juhaina Ali Salim Al Yaqoobi <Juhaina.AlYaqoobi@MTC.EDU.OM>
2020-06-25 16:00:35 +0400

    End of section 11

--- DatingApp.API/Helpers/AutoMapperProfiles.cs
+++ DatingApp.API/Helpers/AutoMapperProfiles.cs
@@ -21,6 +21,8 @@ namespace DatingApp.API.Helpers
                opt.MapFrom(src => src.DateOfBirth.CaculateAge()));
             CreateMap<Photo, PhotosForDetailedDto>();
             CreateMap<UserForUpdateDto, User>();
+            CreateMap<Photo, PhotoForReturnDto>();
+            CreateMap<PhotoForCreationDto, Photo>();
         }
     }
 }
